94159de77468ef37c05a02c2ad3d76e0ed23f3c0
harshjari <thatsharsh@gmail.com>
2012-06-25 13:00:10 -0700

    created new branch with xcode project

---
+++ iOS/ExampleAdvertiserSDK/ExampleAdvertiserSDK/lib/VGAnalytics.h
@@ -0,0 +1,13 @@
+//
+//  VGAnalytics.h
+//  ExampleAdvertiserSDK
+//
+//  Created by Harsh Jariwala on 6/25/12.
+//  Copyright (c) 2012 Carnegie Mellon University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VGAnalytics : NSObject
+
+@end
---
+++ iOS/ExampleAdvertiserSDK/ExampleAdvertiserSDK/lib/VGAnalytics.m
@@ -0,0 +1,13 @@
+//
+//  VGAnalytics.m
+//  ExampleAdvertiserSDK
+//
+//  Created by Harsh Jariwala on 6/25/12.
+//  Copyright (c) 2012 Carnegie Mellon University. All rights reserved.
+//
+
+#import "VGAnalytics.h"
+
+@implementation VGAnalytics
+
+@end
