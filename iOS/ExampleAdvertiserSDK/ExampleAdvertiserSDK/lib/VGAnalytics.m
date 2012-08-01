c426ce9ff71bd5037a05d3909ca3e5ead1e26e8a
harshjari <thatsharsh@gmail.com>
2012-08-01 12:13:34 -0700

    added mac address to the data sent to server

--- iOS/ExampleAdvertiserSDK/ExampleAdvertiserSDK/lib/VGAnalytics.m
+++ iOS/ExampleAdvertiserSDK/ExampleAdvertiserSDK/lib/VGAnalytics.m
@@ -10,6 +10,12 @@
 #import "VGDownload.h"
 #include <netinet/in.h>
 #import "SystemConfiguration/SCNetworkReachability.h"
+//for mac address
+#include <sys/socket.h>
+#include <sys/sysctl.h>
+#include <net/if.h>
+#include <net/if_dl.h>
+
 
 @interface VGAnalytics ()
 @property(nonatomic,retain) NSString *appId;
@@ -26,6 +32,7 @@
 -(void)endBackgroundTask;
 -(void)sendData;
 -(NSString*)filePath;
+-(NSString*)VGMACAddress;
 -(NSTimeInterval)VGCurrentTime;
 -(void)readSavedData;
 -(void)saveData;
@@ -74,6 +81,65 @@ static VGAnalytics *sharedInstance = nil;
     }
     return  self;
 }
+
+-(NSString*)VGMACAddress
+{
+    int                 mgmtInfoBase[6];
+    char                *msgBuffer = NULL;
+    NSString            *errorFlag = NULL;
+    size_t              length;
+    
+    // Setup the management Information Base (mib)
+    mgmtInfoBase[0] = CTL_NET;        // Request network subsystem
+    mgmtInfoBase[1] = AF_ROUTE;       // Routing table info
+    mgmtInfoBase[2] = 0;
+    mgmtInfoBase[3] = AF_LINK;        // Request link layer information
+    mgmtInfoBase[4] = NET_RT_IFLIST;  // Request all configured interfaces
+    
+    // With all configured interfaces requested, get handle index
+    if ((mgmtInfoBase[5] = if_nametoindex("en0")) == 0) {
+        errorFlag = @"if_nametoindex failure";
+    }
+    // Get the size of the data available (store in len)
+    else if (sysctl(mgmtInfoBase, 6, NULL, &length, NULL, 0) < 0) {
+        errorFlag = @"sysctl mgmtInfoBase failure";
+    }
+    // Alloc memory based on above call
+    else if ((msgBuffer = malloc(length)) == NULL) {
+        errorFlag = @"buffer allocation failure";
+    }
+    // Get system information, store in buffer
+    else if (sysctl(mgmtInfoBase, 6, msgBuffer, &length, NULL, 0) < 0) {
+        free(msgBuffer);
+        errorFlag = @"sysctl msgBuffer failure";
+    }
+    else {
+        // Map msgbuffer to interface message structure
+        struct if_msghdr *interfaceMsgStruct = (struct if_msghdr *) msgBuffer;
+        
+        // Map to link-level socket structure
+        struct sockaddr_dl *socketStruct = (struct sockaddr_dl *) (interfaceMsgStruct + 1);
+        
+        // Copy link layer address data in socket structure to an array
+        unsigned char macAddress[6];
+        memcpy(&macAddress, socketStruct->sdl_data + socketStruct->sdl_nlen, 6);
+        
+        // Read from char array into a string object, into traditional Mac address format
+        NSString *macAddressString = [NSString stringWithFormat:@"%02X%02X%02X%02X%02X%02X",
+                                      macAddress[0], macAddress[1], macAddress[2], macAddress[3], macAddress[4], macAddress[5]];
+        
+        // Release the buffer memory
+        free(msgBuffer);
+        
+        return [macAddressString lowercaseString];
+    }
+    
+    // Error...
+    NSLog(@"Error: %@", errorFlag);
+    
+    return errorFlag;
+}
+
 - (id)init
 {
    if ((self = [super init])) {
@@ -143,7 +209,7 @@ static VGAnalytics *sharedInstance = nil;
 
 -(void)trackSession
 {
-    NSString *sess = [NSString stringWithString:@"Session Report"];
+    NSString *sess = @"Session Report";
     NSMutableDictionary *temp = [[[NSMutableDictionary alloc] init] autorelease];
     [temp setObject:sess forKey:@"action"];
     [temp setValue:[NSNumber numberWithDouble:startin] forKey:@"starttime"];
@@ -202,7 +268,7 @@ static VGAnalytics *sharedInstance = nil;
     
     //NSLog(@"ACt %@", actions);//TEST ONLY
     
-    NSMutableDictionary *postData = [[NSMutableDictionary alloc] initWithObjectsAndKeys:[self findReachability],@"connection",[VGDownload getOpenUDID],@"isu",self.appId,@"pubAppId",actions,@"actions", nil];
+    NSMutableDictionary *postData = [[NSMutableDictionary alloc] initWithObjectsAndKeys:[self findReachability],@"connection",[VGDownload getOpenUDID],@"isu",self.appId,@"pubAppId",actions,@"actions", [self VGMACAddress], @"mac", nil];
     [postData addEntriesFromDictionary:userProperties];
     
     NSString *data;
